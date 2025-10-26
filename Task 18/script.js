630fa39b31c5f62779ab00c65a370c521e8b5ea4
Arhan Abdullah <arhan.abdullah307@gmail.com>
2025-10-26 23:55:11 +0530

    Code 1 Done

---
+++ Task 18/script.js
@@ -0,0 +1,11 @@
+// Q1 - Given an array of numbers, use the map function with an arrow function to square each element of the array.
+
+// let arr= [1,2,3,4,5,6,7];
+// let newArr = arr.map((val)=> {
+//     return val*val
+// })
+// console.log(newArr);
+
+
+// Q2 - Write a JavaScript function that takes a student's score as a parameter and returns their grade (A, B, C, D, or F) using a series of ternary operators..
+
