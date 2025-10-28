08649257d8b51d53edd3972c7d392552b1a66531
Arhan Abdullah <arhan.abdullah307@gmail.com>
2025-10-28 23:15:42 +0530

    Initial Commit
    git commit --amend -m "Your new commit message"

---
+++ React/css-module/src/components/button/button.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import styles from './Button.module.css'
+const button = () => {
+  return (
+    <div>
+      <button className={styles.btn}>Click Here</button>
+    </div>
+  )
+}
+
+export default button
