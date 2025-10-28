2b7046ace13a117c0a44d73b8c038984349a1e58
Arhan Abdullah <arhan.abdullah307@gmail.com>
2025-10-28 23:14:43 +0530

    Completed the Project

--- React/ReactBasics_3/src/Components/Cards.jsx
+++ React/ReactBasics_3/src/Components/Cards.jsx
@@ -1,28 +1,30 @@
 import React from "react";
 import { Bookmark } from 'lucide-react';
-const Cards = () => {
+const Cards = (props) => {
     return (
         <div>
             <div className="parent">
                 <div className="card">
-                    <div className="top">
-                        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTyr_r3nj5XBwlhbKTIirZIKPmsMG6e7b7KFyTUxSfyNWX0jP7O2sHcTVo&s" alt="Amazon Logo Image" />
+                    <div>
+                        <div className="top">
+                        <img src={props.brandLogo} alt= {props.brandName} />
                         <button>Save<Bookmark size={14} /></button>
                     </div>
                     <div className="center">
-                        <h3>Amazon <span> 6 days ago</span></h3>
+                        <h3>{props.brandName} <span> {props.datePosted}</span></h3>
                         
-                        <h2>Senior UI/UX Designer</h2>
+                        <h2>{props.position}</h2>
                         <div className="tag">
-                            <h4>Part-Time </h4>
-                            <h4>Senior Level </h4>
+                            <h4>{props.tag1} </h4>
+                            <h4>{props.tag2} </h4>
                         </div>
+                    </div>
                     </div>
 
                     <div className="bottom">
                         <div>
-                            <h3>$120/hr</h3>
-                            <p>Mumbai, India</p>
+                            <h3>{props.paymentPerHour}</h3>
+                            <p>{props.location}</p>
                         </div>
                         <button>Apply Now</button>
                     </div>
