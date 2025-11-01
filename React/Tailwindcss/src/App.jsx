cc20c394b637d9fbe982ff96e4b29323899d95be
Arhan Abdullah <arhan.abdullah307@gmail.com>
2025-11-01 22:46:36 +0530

    Initial commit

--- React/Tailwindcss/src/App.jsx
+++ React/Tailwindcss/src/App.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
+import Section1 from './Components/Section 1/section1'
+import Section2 from './Components/Section 2/section2'
 
 const App = () => {
   return (
     <div>
-      Appp
+      <Section1 />
+      <Section2 />
     </div>
   )
 }
---
+++ React/Tailwindcss/src/Components/Section 1/section1.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Navbar from './navbar'
+const section1 = () => {
+  return (
+    <div className='h-screen w-full font-bold '>
+      <Navbar />
+      Section 1
+    </div>
+  )
+}
+
+export default section1
